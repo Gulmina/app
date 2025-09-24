aac440aee546e6067e07e1a2d82de388e222c039
gulmina <gulmina44@gmail.com>
2025-09-24 11:57:30 +0200

    basic layout added

---
+++ app/src/App.jsx
@@ -0,0 +1,69 @@
+
+import './App.css'
+
+
+import { Flex, Layout } from 'antd';
+import React from 'react';
+
+
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: 'hidden',
+  width: 'calc(50% - 8px)',
+  maxWidth: 'calc(50% - 8px)',
+};
+
+function App() {
+
+ 
+
+
+  return (
+    <>
+
+<Flex gap="middle" wrap>
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    </Flex>
+     {/*  <div className=" text-3xl text-center">
+  Centered Text
+
+
+    </div> */}
+    </>
+  )
+}
+
+export default App
